f07a37bd9045dfec83dccde3d5a02c8f4b253626
Mark Lewis <mark.lewis@zatzy.co.uk>
2024-01-03 21:28:09 +0000

    Update config_flow.py

--- custom_components/leddmx_ble/config_flow.py
+++ custom_components/leddmx_ble/config_flow.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 
 from bluetooth_data_tools import human_readable_name
-from led_ble import BLEAK_EXCEPTIONS, LEDBLE
+from led-dmx-ble import BLEAK_EXCEPTIONS, LEDBLE
 import voluptuous as vol
 
 from homeassistant import config_entries
